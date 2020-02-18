69ccf10524f723bb8f1b1a58aabc911039951f46
darkdante2209 <darkdante2209@gmail.com>
2020-02-18 10:05:57 +0700

    [1.0.0] Connect & create new item MongoDB.

---
+++ sh/env.sh
@@ -0,0 +1,11 @@
+# Config database environment variable 
+export DB_CONNECTION=mongodb
+export DB_HOST=localhost
+export DB_PORT=27017
+export DB_NAME=huychat
+export DB_USERNAME=""
+export DB_PASSWORD=""
+
+# Config app environment variable
+export APP_HOST=localhost
+export APP_PORT=2209
