660d8ab6ce3bf5d456aebd10ffc9d3178851c9a7
darkdante2209 <darkdante2209@gmail.com>
2020-03-30 21:06:53 +0700

    [1.0.0]Add static pages.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine"
 
+
+//Init app
 let app = express();
 
 // Connect to MongoDB
 ConnectDB();
 
-app.get("/test-database", async (req,res) => {
-  try {
-    let item = {
-      userId: "412414124",
-      contactId: "4141124"
-    };
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (err) {
-    console.log(err);
-  }
+// Config view engine
+configViewEngine(app);
+
+app.get("/", (req,res) => {
+  return res.render("main/master");
+});
+
+app.get("/login-register", (req,res) => {
+  return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
