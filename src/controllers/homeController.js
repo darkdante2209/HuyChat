eca0127bcedb1a91f445b86ea7eca389ddaae10a
darkdante2209 <darkdante2209@gmail.com>
2020-05-25 11:10:49 +0700

    [1.0.0] Update avatar and user info front-end

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,8 @@
 let getHome = (req,res) => {
   return res.render("main/home/home", {
     errors:req.flash("errors"),
-    success: req.flash("success")
+    success: req.flash("success"),
+    user: req.user //Như trong passport user thì ta đã lưu user vào trong req nên không cần phải querry về database nữa
   });
 };
 
