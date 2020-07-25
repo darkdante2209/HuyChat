9f7217e395bbb99097860d80c3e42fc1349e01b1
darkdante2209 <darkdante2209@gmail.com>
2020-07-25 22:12:02 +0700

    [1.0.0] Count unread notification & design notification modal

--- src/public/js/addContact.js
+++ src/public/js/addContact.js
@@ -13,13 +13,13 @@ function addContact() {
 }
 //socket lắng nghe sự kiện server gửi về
 socket.on("response-add-new-contact", function(user) {
-    let notif = `<span class="notif-readed-false" data-uid="${user.id}">
+    let notif = `<div class="notif-readed-false" data-uid="${user.id}">
                     <img class="avatar-small" src="images/users/${user.avatar}" alt=""> 
                     <strong>${user.username}</strong> đã gửi cho bạn một lời mời kết bạn!
-                </span><br><br><br>`;
+                </div>`;
     //DOM, paste dữ liệu vào cấu trúc trang web
-    $(".noti_content").prepend(notif);//prepend đẩy thông báo mới nhất từ trên xuống
-
+    $(".noti_content").prepend(notif);//prepend đẩy thông báo mới nhất từ trên xuống - thông báo popup
+    $("ul.list-notifications").prepend(`<li>${notif}</li>`);//Modal bảng thông báo
     increaseNumberNotifContact("count-request-contact-received");
 
     increaseNumberNotification("noti_contact_counter");
