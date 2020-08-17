13e321e4b59531874bd015dc0f55f058b61e407a
darkdante2209 <darkdante2209@gmail.com>
2020-08-18 01:00:34 +0700

    [1.0.0] Chat Image part 01

--- lang/vi.js
+++ lang/vi.js
@@ -22,7 +22,9 @@ export const transErrors = {
   avatar_size: "Ảnh upload tối đa cho phép là 1MB",
   account_undefined: "Tài khoản không tồn tại.",
   user_current_password_failed: "Mật khẩu hiện tại không chính xác.",
-  conversation_not_found: "Cuộc trò chuyện không tồn tại."
+  conversation_not_found: "Cuộc trò chuyện không tồn tại.",
+  image_message_type: "Kiểu file không hợp lệ, chỉ chấp nhận jpg, jpeg hoặc png.",
+  image_message_size: "Ảnh upload tối đa cho phép là 1MB",
 };
 
 export const transSuccess = {
