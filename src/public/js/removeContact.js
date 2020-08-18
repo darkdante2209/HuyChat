e6e547a2675362a705dcd8545fd1e04349e1b87c
darkdante2209 <darkdante2209@gmail.com>
2020-08-19 01:36:52 +0700

    [1.0.0] Handle chat after approve or remove contact

--- src/public/js/removeContact.js
+++ src/public/js/removeContact.js
@@ -23,9 +23,31 @@ function removeContact() {
                     if (data.success) {
                         $("#contacts").find(`ul li[data-uid = ${targetId}]`).remove();         
                         decreaseNumberNotifContact("count-contacts");//js/caculateNotifContact.js
-                        //xóa user phần chat
-                        
                         socket.emit("remove-contact", {contactId: targetId});
+                        
+                        //xóa user phần chat
+                        //All step sau khi xóa contact
+
+                        //Step 0: check active:
+                        let checkActive = $("#all-chat").find(`li[data-chat = ${targetId}]`).hasClass("active");
+                        //Step 1:
+                        $("#all-chat").find(`ul a[href = "#uid_${targetId}"]`).remove();
+                        $("#user-chat").find(`ul a[href = "#uid_${targetId}"]`).remove();
+
+                        //Step 2: Remove rightSide.ejs
+                        $("#screen-chat").find(`div#to_${targetId}`).remove();
+
+                        //Step 3: remove image modal
+                        $("body").find(`div#imagesModal_${targetId}`).remove();
+
+                        //Step 4: remove attachment modal
+                        $("body").find(`div#attachmentsModal_${targetId}`).remove();
+
+                        //Step 5:  click vào cuộc trò chuyện đầu
+                        if (checkActive) {
+                            $("ul.people").find("a")[0].click();
+                        }
+                        
                     }
                 }
             });
@@ -38,6 +60,28 @@ socket.on("response-remove-contact", function(user) {
     $("#contacts").find(`ul li[data-uid = ${user.id}]`).remove();         
     decreaseNumberNotifContact("count-contacts");//js/caculateNotifContact.js
     //xóa user phần chat
+
+    //All step sau khi xóa contact
+
+    //Step 0: check active:
+    let checkActive = $("#all-chat").find(`li[data-chat = ${user.id}]`).hasClass("active");
+    //Step 1:
+    $("#all-chat").find(`ul a[href = "#uid_${user.id}"]`).remove();
+    $("#user-chat").find(`ul a[href = "#uid_${user.id}"]`).remove();
+
+    //Step 2: Remove rightSide.ejs
+    $("#screen-chat").find(`div#to_${user.id}`).remove();
+
+    //Step 3: remove image modal
+    $("body").find(`div#imagesModal_${user.id}`).remove();
+
+    //Step 4: remove attachment modal
+    $("body").find(`div#attachmentsModal_${user.id}`).remove();
+    
+    //Step 5:  click vào cuộc trò chuyện đầu
+    if (checkActive) {
+        $("ul.people").find("a")[0].click();
+    }
 });
 
 $(document).ready(function() {
